363c04405f945af3ba47ec65d984703be7e4c61a
Радмир <57636817+g1xus@users.noreply.github.com>
2020-08-07 17:04:37 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".header-toggle").click(function(){$(".header-wrapper").toggleClass("header-wrapper_active"),$(".header-toggle__item").toggleClass("header-toggle__item_active")}),$('[data-toggle="tooltip"]').tooltip()});
